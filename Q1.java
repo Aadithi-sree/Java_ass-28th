bf6ff517dc0aa68a82f0919b265704f66d4241ac
Aadithi-sree <aadithisreed@gmail.com>
2025-10-30 13:39:16 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,19 @@
+public class Q1
+{
+static int a=20;
+static int b=30;
+public static int average()
+{
+return sum(a,b);
+}
+
+public static int sum(int a,int b)
+{
+return a+b;
+}
+
+public static void main(String[] args)
+{
+System.out.println(average());
+}
+}
